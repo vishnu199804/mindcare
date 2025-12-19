f244a067c0c319c701f3d3b9df0e6a2378afa1de
vishnu199804 <vishnuramesh1804@gmail.com>
2025-12-19 16:19:17 +0530

    Prepare project for Render hosting

--- mindcare/mindcare/settings.py
+++ mindcare/mindcare/settings.py
@@ -12,11 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-RAZORPAY_KEY_ID = 'rzp_test_RqGNErgBrx0DOA'
-RAZORPAY_KEY_SECRET = 'LVrBysgVe22ZDqIDsXMKis0l'
-
+RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
+RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
 
 # ---------------------------------------------------------
 # APPLICATIONS (Correct Order for Custom User Model)
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
 # ---------------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +119,9 @@ USE_TZ = True
 # ---------------------------------------------------------
 # STATIC FILES
 # ---------------------------------------------------------
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
@@ -134,3 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CUSTOM USER MODEL
 # ---------------------------------------------------------
 AUTH_USER_MODEL = "accounts.User"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
